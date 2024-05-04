d4c2e815eff1d8d242f5fc729b55ca8153f2e9f1
Alexander Zuev <azuev@outlook.com>
2024-05-04 12:43:41 +0400

    Trying again

--- ai_on_the_go/bot.py
+++ ai_on_the_go/bot.py
@@ -46,10 +46,7 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # Configure langchain groq client
-llm = ChatGroq(
-    temperature=0.8,
-    groq_api_key=GROQ_API_KEY,
-    model_name="llama3-70b-8192")
+llm = ChatGroq(temperature=0.8, groq_api_key=GROQ_API_KEY, model_name="llama3-70b-8192")
 
 # Dictionary to manage conversation for each user
 conversations = defaultdict(lambda: None)  #
@@ -88,9 +85,7 @@ async def startup():
 
 # Command handler for /start
 async def start(update: Update, context):
-    logger.debug(
-        "Received /start command from user %s",
-        update.effective_chat.id)
+    logger.debug("Received /start command from user %s", update.effective_chat.id)
     try:
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
@@ -105,8 +100,7 @@ async def start(update: Update, context):
 async def handle_message(update: Update, context):
     user_text = update.message.text  # extract text from the user message
     user_id = update.effective_chat.id
-    logger.debug("Received message from user %s: %s",
-                 update.effective_chat.id, user_text)
+    logger.debug("Received message from user %s: %s", update.effective_chat.id, user_text)
 
     # check if user has a conversation
     if conversations[user_id] is None:
@@ -115,10 +109,7 @@ async def handle_message(update: Update, context):
     try:
         chat_response = await get_llm_response(conversations[user_id], user_text)
         await context.bot.send_message(chat_id=update.effective_chat.id, text=chat_response)
-        logger.debug(
-            "Sent response to user %s: %s",
-            update.effective_chat.id,
-            chat_response)
+        logger.debug("Sent response to user %s: %s", update.effective_chat.id, chat_response)
     except Exception as e:
         logger.error("Error during message handling: %s", str(e))
         raise e
@@ -126,11 +117,7 @@ async def handle_message(update: Update, context):
 
 # Add handlers to the application
 application.add_handler(CommandHandler("start", start))
-application.add_handler(
-    MessageHandler(
-        filters.TEXT & (
-            ~filters.COMMAND),
-        handle_message))
+application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
 
 
 # Setup the webhook
@@ -147,8 +134,7 @@ async def webhook_updates(request: Request):
     except Exception as e:
         # log the error and send it back in the response for the debugging
         logger.error(f"Failed to process update: {str(e)}", exc_info=True)
-        return JSONResponse(status_code=500, content={
-                            "message": "Bad Request: Invalid data", "error": str(e)})
+        return JSONResponse(status_code=500, content={"message": "Bad Request: Invalid data", "error": str(e)})
 
 
 # Run the app using Uvicorn, if the script is run directly
